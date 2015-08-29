c79f7138b23b9fcf3fe1831a0817d93a9fa29886
Oliver Lehmann <lehmann@ans-netz.de>
2015-08-29 21:28:54 +0200

    Profiles not needed until now

--- src/main/java/org/laladev/moneyjinn/server/config/DatabaseConfiguration.java
+++ src/main/java/org/laladev/moneyjinn/server/config/DatabaseConfiguration.java
@@ -4,19 +4,16 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.laladev.moneyjinn.api.MoneyjinnProfiles;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @MapperScan("org.laladev.moneyjinn.businesslogic.dao.mapper")
-@Profile("!" + MoneyjinnProfiles.TEST)
 public class DatabaseConfiguration {
 	@Inject
 	private DataSource pool;
