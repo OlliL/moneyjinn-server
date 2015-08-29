f209b45b7c8ec7391b70c5fa1e6802bcd36a3216
Oliver Lehmann <lehmann@ans-netz.de>
2015-08-29 21:36:19 +0200

    make testing easier with some helpermethods

--- src/test/java/org/laladev/moneyjinn/server/controller/AbstractControllerTest.java
+++ src/test/java/org/laladev/moneyjinn/server/controller/AbstractControllerTest.java
@@ -1,18 +1,24 @@
 package org.laladev.moneyjinn.server.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.io.IOException;
 
 import javax.inject.Inject;
 
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.laladev.moneyjinn.api.MoneyjinnProfiles;
 import org.laladev.moneyjinn.server.config.MoneyjinnConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -23,18 +29,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @WebAppConfiguration
 @EnableAutoConfiguration
 @ActiveProfiles(MoneyjinnProfiles.TEST)
-public class AbstractControllerTest {
+public abstract class AbstractControllerTest {
 	@Inject
 	private ObjectMapper objectMapper;
 	@Inject
 	private MockMvc mvc;
 
-	public MockMvc mvc() {
+	private MockMvc mvc() {
 		return this.mvc;
 	}
 
-	protected <T> T map(final String string, final Class<T> clazz)
+	private <T> T map(final String string, final Class<T> clazz)
 			throws JsonParseException, JsonMappingException, IOException {
 		return this.objectMapper.readValue(string, clazz);
+	}
+
+	protected abstract String getDomain();
+
+	protected <T> T callUsecaseStatusOkWithResponse(final String uri, final Class<T> clazz) throws Exception {
+		final MvcResult result = this.mvc().perform(
+				MockMvcRequestBuilders.get("/moneyflow/" + this.getDomain() + uri).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andReturn();
+
+		final String content = result.getResponse().getContentAsString();
+
+		Assert.assertNotNull(content);
+
+		final T actual = this.map(content, clazz);
+
+		return actual;
+
 	}
 }
--- src/test/java/org/laladev/moneyjinn/server/controller/UserControllerTest.java
+++ src/test/java/org/laladev/moneyjinn/server/controller/UserControllerTest.java
@@ -1,7 +1,5 @@
 package org.laladev.moneyjinn.server.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,39 +11,25 @@ import org.laladev.moneyjinn.core.rest.model.user.ShowEditUserResponse;
 import org.laladev.moneyjinn.server.builder.AccessRelationTransportBuilder;
 import org.laladev.moneyjinn.server.builder.GroupTransportBuilder;
 import org.laladev.moneyjinn.server.builder.UserTransportBuilder;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 public class UserControllerTest extends AbstractControllerTest {
 
+	@Override
+	protected String getDomain() {
+		return "user";
+	}
+
 	@Test
 	public void showEditUser_unknownUser_emptyResponseObject() throws Exception {
-		final MvcResult result = super.mvc().perform(
-				MockMvcRequestBuilders.get("/moneyflow/user/showEditUser/666").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andReturn();
-
-		final String content = result.getResponse().getContentAsString();
-
-		Assert.assertNotNull(content);
-
 		final ShowEditUserResponse expected = new ShowEditUserResponse();
-		final ShowEditUserResponse actual = super.map(content, ShowEditUserResponse.class);
+		final ShowEditUserResponse actual = super.callUsecaseStatusOkWithResponse("/showEditUser/666",
+				ShowEditUserResponse.class);
 
 		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
 	public void showEditUser_User1_completeResponseObject() throws Exception {
-		final MvcResult result = super.mvc()
-				.perform(
-						MockMvcRequestBuilders.get("/moneyflow/user/showEditUser/3").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andReturn();
-
-		final String content = result.getResponse().getContentAsString();
-
-		Assert.assertNotNull(content);
-
 		final ShowEditUserResponse expected = new ShowEditUserResponse();
 		expected.setUserTransport(new UserTransportBuilder().forUser1().build());
 		final List<GroupTransport> groupTransports = new ArrayList<>();
@@ -60,8 +44,10 @@ public class UserControllerTest extends AbstractControllerTest {
 		accessRelationTransports.add(new AccessRelationTransportBuilder().forUser1_2800_01_01().build());
 		expected.setAccessRelationTransports(accessRelationTransports);
 
-		final ShowEditUserResponse actual = super.map(content, ShowEditUserResponse.class);
+		final ShowEditUserResponse actual = super.callUsecaseStatusOkWithResponse("/showEditUser/3",
+				ShowEditUserResponse.class);
 
 		Assert.assertEquals(expected, actual);
 	}
+
 }
