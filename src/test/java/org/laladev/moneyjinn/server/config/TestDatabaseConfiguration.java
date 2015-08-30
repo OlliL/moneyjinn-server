66ef653f7f9bcca4e39abb37cb3f4dba1fc43052
Oliver Lehmann <lehmann@ans-netz.de>
2015-08-30 12:52:22 +0200

    clean cache before each Testrun makes it work

--- src/test/java/org/laladev/moneyjinn/server/config/TestDatabaseConfiguration.java
+++ src/test/java/org/laladev/moneyjinn/server/config/TestDatabaseConfiguration.java
@@ -1,46 +1,18 @@
 package org.laladev.moneyjinn.server.config;
 
-import org.h2.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 public class TestDatabaseConfiguration {
-	// @Bean
-	// public EmbeddedDatabase dataSource() {
-	// return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("h2dump.sql")
-	// .addScript("h2ext.sql").build();
-	// }
 
 	@Bean
-	public SimpleDriverDataSource simpleDriverDataSource() {
-		final SimpleDriverDataSource ds = new SimpleDriverDataSource();
-		ds.setDriverClass(Driver.class);
-		ds.setUrl("jdbc:h2:mem:test;MODE=mysql;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-		ds.setUsername("sa");
-		ds.setPassword("");
-
-		return ds;
+	public EmbeddedDatabase dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("h2dump.sql")
+				.addScript("h2ext.sql").build();
 	}
 
-	// @Value("classpath:h2dump.sql")
-	// private Resource schemaScript;
-	//
-	// @Value("classpath:h2ext.sql")
-	// private Resource dataScript;
-	// @Bean
-	// public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
-	// final DataSourceInitializer initializer = new DataSourceInitializer();
-	// initializer.setDataSource(dataSource);
-	// initializer.setDatabasePopulator(databasePopulator());
-	// return initializer;
-	// }
-	//
-	// private DatabasePopulator databasePopulator() {
-	// final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-	// populator.addScript(schemaScript);
-	// populator.addScript(dataScript);
-	// return populator;
-	// }
 }
--- src/test/java/org/laladev/moneyjinn/server/controller/AbstractControllerTest.java
+++ src/test/java/org/laladev/moneyjinn/server/controller/AbstractControllerTest.java
@@ -2,16 +2,17 @@ package org.laladev.moneyjinn.server.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
-
 import javax.inject.Inject;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.laladev.moneyjinn.api.MoneyjinnProfiles;
 import org.laladev.moneyjinn.server.config.MoneyjinnConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
@@ -24,42 +25,39 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { MoneyjinnConfiguration.class })
 @WebAppConfiguration
 @EnableAutoConfiguration
 @ActiveProfiles(MoneyjinnProfiles.TEST)
-@SqlGroup({ @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:h2prepare.sql",
-		"classpath:h2dump.sql", "classpath:h2ext.sql", "classpath:h2defaults.sql", "classpath:testdata.sql" }) })
+@SqlGroup({ @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:h2defaults.sql",
+		"classpath:testdata.sql" }) })
 public abstract class AbstractControllerTest {
 	@Inject
 	private ObjectMapper objectMapper;
 	@Inject
 	private MockMvc mvc;
-
-	private MockMvc mvc() {
-		return this.mvc;
-	}
-
-	private <T> T map(final String string, final Class<T> clazz)
-			throws JsonParseException, JsonMappingException, IOException {
-		return this.objectMapper.readValue(string, clazz);
-	}
+	@Inject
+	private CacheManager cacheManager;
 
 	protected abstract String getUsecase();
 
+	/**
+	 * Input "user" and "ShowEditUserTest" and get "user/showEditUser" back
+	 *
+	 * @param prefix
+	 * @param clazz
+	 * @return
+	 */
 	protected String getUsecaseFromTestClassName(final String prefix, final Class<?> clazz) {
 		final String usecase = clazz.getSimpleName().replace("Test", "");
 		return prefix + "/" + Character.toLowerCase(usecase.charAt(0)) + usecase.substring(1);
 	}
 
 	protected <T> T callUsecaseWithGET(final String uriParameters, final Class<T> clazz) throws Exception {
-		final MvcResult result = this.mvc().perform(MockMvcRequestBuilders
+		final MvcResult result = this.mvc.perform(MockMvcRequestBuilders
 				.get("/moneyflow/" + this.getUsecase() + uriParameters).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andReturn();
 
@@ -67,7 +65,7 @@ public abstract class AbstractControllerTest {
 
 		Assert.assertNotNull(content);
 
-		final T actual = this.map(content, clazz);
+		final T actual = this.objectMapper.readValue(content, clazz);
 
 		return actual;
 
@@ -82,7 +80,7 @@ public abstract class AbstractControllerTest {
 			status = status().isNoContent();
 		}
 
-		final MvcResult result = this.mvc()
+		final MvcResult result = this.mvc
 				.perform(MockMvcRequestBuilders.put("/moneyflow/" + this.getUsecase() + uriParameters).content(bodyStr)
 						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status).andReturn();
@@ -93,7 +91,7 @@ public abstract class AbstractControllerTest {
 		if (!noResult) {
 			Assert.assertTrue(content.length() > 0);
 
-			final T actual = this.map(content, clazz);
+			final T actual = this.objectMapper.readValue(content, clazz);
 
 			return actual;
 		} else {
@@ -103,4 +101,11 @@ public abstract class AbstractControllerTest {
 
 	}
 
+	/**
+	 * Clears all Caches before each test execution
+	 */
+	@Before
+	public void before() {
+		this.cacheManager.getCacheNames().stream().map(this.cacheManager::getCache).forEach(Cache::clear);
+	}
 }
