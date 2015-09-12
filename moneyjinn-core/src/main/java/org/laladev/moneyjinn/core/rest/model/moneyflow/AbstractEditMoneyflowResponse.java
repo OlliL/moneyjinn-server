3b73c073171a03fc4180de41ffbc7282cc465f0b
Oliver Lehmann <lehmann@ans-netz.de>
2015-09-12 22:53:35 +0200

    more work on moneyflow

--- moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/moneyflow/AbstractEditMoneyflowResponse.java
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/moneyflow/AbstractEditMoneyflowResponse.java
@@ -6,7 +6,6 @@ import org.laladev.moneyjinn.core.rest.model.ValidationResponse;
 import org.laladev.moneyjinn.core.rest.model.transport.CapitalsourceTransport;
 import org.laladev.moneyjinn.core.rest.model.transport.ContractpartnerTransport;
 import org.laladev.moneyjinn.core.rest.model.transport.PostingAccountTransport;
-import org.laladev.moneyjinn.core.rest.model.transport.PreDefMoneyflowTransport;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -36,24 +35,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 //SUCH DAMAGE.
 //
 
-public abstract class AbstractAddMoneyflowResponse extends ValidationResponse {
-	@JsonProperty("preDefMoneyflowTransport")
-	private List<PreDefMoneyflowTransport> preDefMoneyflowTransports;
+public abstract class AbstractEditMoneyflowResponse extends ValidationResponse {
 	@JsonProperty("capitalsourceTransport")
 	private List<CapitalsourceTransport> capitalsourceTransports;
 	@JsonProperty("contractpartnerTransport")
 	private List<ContractpartnerTransport> contractpartnerTransports;
 	@JsonProperty("postingAccountTransport")
 	private List<PostingAccountTransport> postingAccountTransports;
-	private Integer settingNumberOfFreeMoneyflows;
-
-	public final List<PreDefMoneyflowTransport> getPreDefMoneyflowTransports() {
-		return this.preDefMoneyflowTransports;
-	}
-
-	public final void setPreDefMoneyflowTransports(final List<PreDefMoneyflowTransport> preDefMoneyflowTransports) {
-		this.preDefMoneyflowTransports = preDefMoneyflowTransports;
-	}
 
 	public final List<CapitalsourceTransport> getCapitalsourceTransports() {
 		return this.capitalsourceTransports;
@@ -79,28 +67,16 @@ public abstract class AbstractAddMoneyflowResponse extends ValidationResponse {
 		this.postingAccountTransports = postingAccountTransports;
 	}
 
-	public final Integer getSettingNumberOfFreeMoneyflows() {
-		return this.settingNumberOfFreeMoneyflows;
-	}
-
-	public final void setSettingNumberOfFreeMoneyflows(final Integer settingNumberOfFreeMoneyflows) {
-		this.settingNumberOfFreeMoneyflows = settingNumberOfFreeMoneyflows;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
+		int result = super.hashCode();
 		result = prime * result
 				+ ((this.capitalsourceTransports == null) ? 0 : this.capitalsourceTransports.hashCode());
 		result = prime * result
 				+ ((this.contractpartnerTransports == null) ? 0 : this.contractpartnerTransports.hashCode());
 		result = prime * result
 				+ ((this.postingAccountTransports == null) ? 0 : this.postingAccountTransports.hashCode());
-		result = prime * result
-				+ ((this.preDefMoneyflowTransports == null) ? 0 : this.preDefMoneyflowTransports.hashCode());
-		result = prime * result
-				+ ((this.settingNumberOfFreeMoneyflows == null) ? 0 : this.settingNumberOfFreeMoneyflows.hashCode());
 		return result;
 	}
 
@@ -109,13 +85,13 @@ public abstract class AbstractAddMoneyflowResponse extends ValidationResponse {
 		if (this == obj) {
 			return true;
 		}
-		if (obj == null) {
+		if (!super.equals(obj)) {
 			return false;
 		}
 		if (this.getClass() != obj.getClass()) {
 			return false;
 		}
-		final AbstractAddMoneyflowResponse other = (AbstractAddMoneyflowResponse) obj;
+		final AbstractEditMoneyflowResponse other = (AbstractEditMoneyflowResponse) obj;
 		if (this.capitalsourceTransports == null) {
 			if (other.capitalsourceTransports != null) {
 				return false;
@@ -137,36 +113,18 @@ public abstract class AbstractAddMoneyflowResponse extends ValidationResponse {
 		} else if (!this.postingAccountTransports.equals(other.postingAccountTransports)) {
 			return false;
 		}
-		if (this.preDefMoneyflowTransports == null) {
-			if (other.preDefMoneyflowTransports != null) {
-				return false;
-			}
-		} else if (!this.preDefMoneyflowTransports.equals(other.preDefMoneyflowTransports)) {
-			return false;
-		}
-		if (this.settingNumberOfFreeMoneyflows == null) {
-			if (other.settingNumberOfFreeMoneyflows != null) {
-				return false;
-			}
-		} else if (!this.settingNumberOfFreeMoneyflows.equals(other.settingNumberOfFreeMoneyflows)) {
-			return false;
-		}
 		return true;
 	}
 
 	@Override
 	public String toString() {
 		final StringBuilder builder = new StringBuilder();
-		builder.append("AbstractAddMoneyflowResponse [preDefMoneyflowTransports=");
-		builder.append(this.preDefMoneyflowTransports);
-		builder.append(", capitalsourceTransports=");
+		builder.append("AbstractEditMoneyflowResponse [capitalsourceTransports=");
 		builder.append(this.capitalsourceTransports);
 		builder.append(", contractpartnerTransports=");
 		builder.append(this.contractpartnerTransports);
 		builder.append(", postingAccountTransports=");
 		builder.append(this.postingAccountTransports);
-		builder.append(", settingNumberOfFreeMoneyflows=");
-		builder.append(this.settingNumberOfFreeMoneyflows);
 		builder.append("]");
 		return builder.toString();
 	}
---
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/moneyflow/UpdateMoneyflowRequest.java
@@ -0,0 +1,85 @@
+package org.laladev.moneyjinn.core.rest.model.moneyflow;
+
+import org.laladev.moneyjinn.core.rest.model.AbstractRequest;
+import org.laladev.moneyjinn.core.rest.model.transport.MoneyflowTransport;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+//
+//Copyright (c) 2015 Oliver Lehmann <oliver@laladev.org>
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+//
+
+@JsonRootName("updateMoneyflowRequest")
+public class UpdateMoneyflowRequest extends AbstractRequest {
+	private MoneyflowTransport moneyflowTransport;
+
+	public final MoneyflowTransport getMoneyflowTransport() {
+		return this.moneyflowTransport;
+	}
+
+	public final void setMoneyflowTransport(final MoneyflowTransport moneyflowTransport) {
+		this.moneyflowTransport = moneyflowTransport;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.moneyflowTransport == null) ? 0 : this.moneyflowTransport.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final UpdateMoneyflowRequest other = (UpdateMoneyflowRequest) obj;
+		if (this.moneyflowTransport == null) {
+			if (other.moneyflowTransport != null) {
+				return false;
+			}
+		} else if (!this.moneyflowTransport.equals(other.moneyflowTransport)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("UpdateMoneyflowRequest [moneyflowTransport=");
+		builder.append(this.moneyflowTransport);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
