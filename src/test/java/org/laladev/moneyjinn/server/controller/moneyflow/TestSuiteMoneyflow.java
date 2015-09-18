b63ea56ffd9af4b0b81b8a11e773d49f638ec777
Oliver Lehmann <lehmann@ans-netz.de>
2015-09-18 23:35:46 +0200

    add missing test to suite

--- src/test/java/org/laladev/moneyjinn/server/controller/moneyflow/TestSuiteMoneyflow.java
+++ src/test/java/org/laladev/moneyjinn/server/controller/moneyflow/TestSuiteMoneyflow.java
@@ -4,8 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CreateMoneyflowsTest.class, DeleteMoneyflowByIdTest.class, ShowDeleteMoneyflowTest.class,
-		ShowEditMoneyflowTest.class, ShowAddMoneyflowsTest.class, UpdateMoneyflowTest.class })
+@Suite.SuiteClasses({ CreateMoneyflowsTest.class, DeleteMoneyflowByIdTest.class, ShowAddMoneyflowsTest.class,
+		ShowDeleteMoneyflowTest.class, ShowEditMoneyflowTest.class, ShowSearchMoneyflowFormTest.class,
+		UpdateMoneyflowTest.class })
 public class TestSuiteMoneyflow {
 
 }
