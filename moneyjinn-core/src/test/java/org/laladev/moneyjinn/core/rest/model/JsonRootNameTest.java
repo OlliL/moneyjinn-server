04f9d99ccde8d88699830c383e77e5e14e773741
Oliver Lehmann <lehmann@ans-netz.de>
2016-08-27 01:22:06 +0200

    JavaDoc

--- moneyjinn-core/src/test/java/org/laladev/moneyjinn/core/rest/model/JsonRootNameTest.java
+++ moneyjinn-core/src/test/java/org/laladev/moneyjinn/core/rest/model/JsonRootNameTest.java
@@ -10,6 +10,13 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 
 public class JsonRootNameTest {
 
+	/**
+	 * This test makes sure all REST-Model classes which are annotated with {@link JsonRootName}
+	 * have JsonRootName matching to their class name. One exception is {@code ErrorResponse} which
+	 * is intended to be named "error" in the REST Response.<br>
+	 * <br>
+	 * This test does not validate that an annotation was specified at all!
+	 */
 	@Test
 	public void testAnnotation() {
 		final Reflections reflections = new Reflections("org.laladev.moneyjinn.core.rest");
