d995c0d2c7de736e3c401ef581096ea6830e0a73
Oliver Lehmann <lehmann@ans-netz.de>
2016-09-10 00:17:05 +0200

    implement Test to check group wide editing while caching

--- src/test/java/org/laladev/moneyjinn/businesslogic/service/impl/ContractpartnerServiceTest.java
+++ src/test/java/org/laladev/moneyjinn/businesslogic/service/impl/ContractpartnerServiceTest.java
@@ -2,13 +2,18 @@ package org.laladev.moneyjinn.businesslogic.service.impl;
 
 import javax.inject.Inject;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.laladev.moneyjinn.AbstractTest;
 import org.laladev.moneyjinn.businesslogic.model.Contractpartner;
+import org.laladev.moneyjinn.businesslogic.model.ContractpartnerID;
 import org.laladev.moneyjinn.businesslogic.model.access.User;
 import org.laladev.moneyjinn.businesslogic.model.access.UserID;
 import org.laladev.moneyjinn.businesslogic.model.exception.BusinessException;
 import org.laladev.moneyjinn.businesslogic.service.api.IContractpartnerService;
+import org.laladev.moneyjinn.server.builder.ContractpartnerTransportBuilder;
+import org.laladev.moneyjinn.server.builder.UserTransportBuilder;
 
 public class ContractpartnerServiceTest extends AbstractTest {
 	@Inject
@@ -24,7 +29,7 @@ public class ContractpartnerServiceTest extends AbstractTest {
 	@Test(expected = BusinessException.class)
 	public void test_createWithInvalidEntity_raisesException() {
 		final Contractpartner contractpartner = new Contractpartner();
-		contractpartner.setUser(new User(new UserID(1l)));
+		contractpartner.setUser(new User(new UserID(1L)));
 
 		this.contractpartnerService.createContractpartner(contractpartner);
 	}
@@ -32,8 +37,36 @@ public class ContractpartnerServiceTest extends AbstractTest {
 	@Test(expected = BusinessException.class)
 	public void test_updateWithInvalidEntity_raisesException() {
 		final Contractpartner contractpartner = new Contractpartner();
-		contractpartner.setUser(new User(new UserID(1l)));
+		contractpartner.setUser(new User(new UserID(1L)));
 
 		this.contractpartnerService.updateContractpartner(contractpartner);
+	}
+
+	@Test
+	@Ignore("this does not work yet - see wiki TODO list")
+	public void test_userAeditsContractpartner_userBsameGroupSeesCachedChange() {
+		final UserID user1ID = new UserID(UserTransportBuilder.USER1_ID);
+		final UserID user2ID = new UserID(UserTransportBuilder.USER2_ID);
+
+		// this caches
+		Contractpartner contractpartner = this.contractpartnerService.getContractpartnerById(user1ID,
+				new ContractpartnerID(ContractpartnerTransportBuilder.CONTRACTPARTNER1_ID));
+
+		contractpartner = this.contractpartnerService.getContractpartnerById(user2ID,
+				new ContractpartnerID(ContractpartnerTransportBuilder.CONTRACTPARTNER1_ID));
+
+		final String name = String.valueOf(System.currentTimeMillis());
+
+		contractpartner.getUser().setId(user2ID);
+		contractpartner.setName(name);
+
+		// this should also modify the cache of user 1!
+		this.contractpartnerService.updateContractpartner(contractpartner);
+
+		// this should now retrieve the changed cache entry!
+		contractpartner = this.contractpartnerService.getContractpartnerById(user1ID,
+				new ContractpartnerID(ContractpartnerTransportBuilder.CONTRACTPARTNER1_ID));
+
+		Assert.assertEquals(name, contractpartner.getName());
 	}
 }
