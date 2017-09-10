9e9cda8d746f73a218bfbec22d1508dbb35bdaec
Oliver Lehmann <lehmann@ans-netz.de>
2017-09-10 22:24:53 +0200

    make contractpartner addable via AJAX

--- moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/contractpartner/CreateContractpartnerResponse.java
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/contractpartner/CreateContractpartnerResponse.java
@@ -1,5 +1,7 @@
 package org.laladev.moneyjinn.core.rest.model.contractpartner;
 
+import org.laladev.moneyjinn.core.rest.model.ValidationResponse;
+
 //
 //Copyright (c) 2015 Oliver Lehmann <oliver@laladev.org>
 //All rights reserved.
@@ -29,6 +31,54 @@ package org.laladev.moneyjinn.core.rest.model.contractpartner;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName("createContractpartnerResponse")
-public class CreateContractpartnerResponse extends AbstractCreateContractpartnerResponse {
+public class CreateContractpartnerResponse extends ValidationResponse {
+	private Long contractpartnerId;
+
+	public final Long getContractpartnerId() {
+		return this.contractpartnerId;
+	}
+
+	public final void setContractpartnerId(final Long contractpartnerId) {
+		this.contractpartnerId = contractpartnerId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((this.contractpartnerId == null) ? 0 : this.contractpartnerId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final CreateContractpartnerResponse other = (CreateContractpartnerResponse) obj;
+		if (this.contractpartnerId == null) {
+			if (other.contractpartnerId != null) {
+				return false;
+			}
+		} else if (!this.contractpartnerId.equals(other.contractpartnerId)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("CreateContractpartnerResponse [contractpartnerId=");
+		builder.append(this.contractpartnerId);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
--- moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/contractpartner/ShowCreateContractpartnerResponse.java
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/contractpartner/ShowCreateContractpartnerResponse.java
@@ -1,5 +1,11 @@
 package org.laladev.moneyjinn.core.rest.model.contractpartner;
 
+import java.util.List;
+
+import org.laladev.moneyjinn.core.rest.model.transport.PostingAccountTransport;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 //
 //Copyright (c) 2015 Oliver Lehmann <oliver@laladev.org>
 //All rights reserved.
@@ -29,6 +35,56 @@ package org.laladev.moneyjinn.core.rest.model.contractpartner;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName("showCreateContractpartnerResponse")
-public class ShowCreateContractpartnerResponse extends AbstractCreateContractpartnerResponse {
+public class ShowCreateContractpartnerResponse {
+	@JsonProperty("postingAccountTransport")
+	private List<PostingAccountTransport> postingAccountTransports;
+
+	public final List<PostingAccountTransport> getPostingAccountTransports() {
+		return this.postingAccountTransports;
+	}
+
+	public final void setPostingAccountTransports(final List<PostingAccountTransport> postingAccountTransports) {
+		this.postingAccountTransports = postingAccountTransports;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((this.postingAccountTransports == null) ? 0 : this.postingAccountTransports.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final ShowCreateContractpartnerResponse other = (ShowCreateContractpartnerResponse) obj;
+		if (this.postingAccountTransports == null) {
+			if (other.postingAccountTransports != null) {
+				return false;
+			}
+		} else if (!this.postingAccountTransports.equals(other.postingAccountTransports)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("ShowCreateContractpartnerResponse [postingAccountTransports=");
+		builder.append(this.postingAccountTransports);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
