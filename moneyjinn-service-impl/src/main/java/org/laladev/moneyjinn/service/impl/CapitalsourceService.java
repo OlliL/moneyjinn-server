73e63b64d2c000162ee2b2192f631554697184fe
Oliver Lehmann <lehmann@ans-netz.de>
2017-09-11 13:57:23 +0200

    fix warnings

--- moneyjinn-service-impl/src/main/java/org/laladev/moneyjinn/service/impl/CapitalsourceService.java
+++ moneyjinn-service-impl/src/main/java/org/laladev/moneyjinn/service/impl/CapitalsourceService.java
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2015-2016 Oliver Lehmann <oliver@laladev.org>
+// Copyright (c) 2015-2017 Oliver Lehmann <oliver@laladev.org>
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -25,40 +25,6 @@
 //
 
 package org.laladev.moneyjinn.service.impl;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-//Copyright (c) 2015 Oliver Lehmann <oliver@laladev.org>
-//All rights reserved.
-//
-//Redistribution and use in source and binary forms, with or without
-//modification, are permitted provided that the following conditions
-//are met:
-//1. Redistributions of source code must retain the above copyright
-//notice, this list of conditions and the following disclaimer
-//2. Redistributions in binary form must reproduce the above copyright
-//notice, this list of conditions and the following disclaimer in the
-//documentation and/or other materials provided with the distribution.
-//
-//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-//SUCH DAMAGE.
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import javax.inject.Inject;
-import javax.inject.Named;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -89,6 +55,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.util.Assert;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Named
 @EnableCaching
 public class CapitalsourceService extends AbstractService implements ICapitalsourceService {
@@ -109,7 +83,7 @@ public class CapitalsourceService extends AbstractService implements ICapitalsou
 
 	}
 
-	private final Capitalsource mapCapitalsourceData(final CapitalsourceData capitalsourceData) {
+	private Capitalsource mapCapitalsourceData(final CapitalsourceData capitalsourceData) {
 		if (capitalsourceData != null) {
 			final Capitalsource capitalsource = super.map(capitalsourceData, Capitalsource.class);
 			final UserID userId = capitalsource.getUser().getId();
@@ -122,8 +96,8 @@ public class CapitalsourceService extends AbstractService implements ICapitalsou
 		return null;
 	}
 
-	private final List<Capitalsource> mapCapitalsourceDataList(final List<CapitalsourceData> capitalsourceDataList) {
-		return capitalsourceDataList.stream().map(element -> this.mapCapitalsourceData(element))
+	private List<Capitalsource> mapCapitalsourceDataList(final List<CapitalsourceData> capitalsourceDataList) {
+		return capitalsourceDataList.stream().map(this::mapCapitalsourceData)
 				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
@@ -133,10 +107,8 @@ public class CapitalsourceService extends AbstractService implements ICapitalsou
 	 *
 	 * @param capitalsource
 	 *            {@link Capitalsource}
-	 *
-	 * @return Capitalsource
 	 */
-	private final void prepareCapitalsource(final Capitalsource capitalsource) {
+	private void prepareCapitalsource(final Capitalsource capitalsource) {
 		if (capitalsource.getValidFrom() == null) {
 			capitalsource.setValidFrom(LocalDate.now());
 		}
@@ -387,7 +359,7 @@ public class CapitalsourceService extends AbstractService implements ICapitalsou
 	public Capitalsource getCapitalsourceByAccount(final UserID userId, final BankAccount bankAccount,
 			final LocalDate date) {
 		final CapitalsourceData capitalsourceData = this.capitalsourceDao
-				.getCapitalsourceByAccount(bankAccount.getBankCode(), bankAccount.getAccountNumber(), endOfMonth);
+				.getCapitalsourceByAccount(bankAccount.getBankCode(), bankAccount.getAccountNumber(), date);
 
 		return this.mapCapitalsourceData(capitalsourceData);
 	}
