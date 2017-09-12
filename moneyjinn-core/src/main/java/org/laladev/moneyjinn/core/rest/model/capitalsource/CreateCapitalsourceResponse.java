19522a7d27da97f7663bd2bb504831e33109bbb7
Oliver Lehmann <lehmann@ans-netz.de>
2017-09-12 19:57:50 +0200

    Make Capitalsources addable via AJAX

---
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/capitalsource/CreateCapitalsourceResponse.java
@@ -0,0 +1,83 @@
+//
+//Copyright (c) 2017 Oliver Lehmann <oliver@laladev.org>
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+//
+
+package org.laladev.moneyjinn.core.rest.model.capitalsource;
+
+import org.laladev.moneyjinn.core.rest.model.ValidationResponse;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName("createCapitalsourceResponse")
+public class CreateCapitalsourceResponse extends ValidationResponse {
+	private Long capitalsourceId;
+
+	public final Long getCapitalsourceId() {
+		return this.capitalsourceId;
+	}
+
+	public final void setCapitalsourceId(final Long capitalsourceId) {
+		this.capitalsourceId = capitalsourceId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((this.capitalsourceId == null) ? 0 : this.capitalsourceId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final CreateCapitalsourceResponse other = (CreateCapitalsourceResponse) obj;
+		if (this.capitalsourceId == null) {
+			if (other.capitalsourceId != null) {
+				return false;
+			}
+		} else if (!this.capitalsourceId.equals(other.capitalsourceId)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("CreateCapitalsourceResponse [capitalsourceId=");
+		builder.append(this.capitalsourceId);
+		builder.append("]");
+		return builder.toString();
+	}
+}
