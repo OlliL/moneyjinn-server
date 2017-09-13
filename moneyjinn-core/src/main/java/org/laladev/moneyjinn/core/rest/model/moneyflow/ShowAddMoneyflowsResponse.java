559e89dabe2a890fbf2885d6e1a12ff9f6ed7d8d
Oliver Lehmann <lehmann@ans-netz.de>
2017-09-13 19:54:01 +0200

    Create Moneyflows with AJAX

--- moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/moneyflow/ShowAddMoneyflowsResponse.java
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/moneyflow/ShowAddMoneyflowsResponse.java
@@ -26,9 +26,130 @@
 
 package org.laladev.moneyjinn.core.rest.model.moneyflow;
 
+import java.util.List;
+
+import org.laladev.moneyjinn.core.rest.model.transport.CapitalsourceTransport;
+import org.laladev.moneyjinn.core.rest.model.transport.ContractpartnerTransport;
+import org.laladev.moneyjinn.core.rest.model.transport.PostingAccountTransport;
+import org.laladev.moneyjinn.core.rest.model.transport.PreDefMoneyflowTransport;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName("showAddMoneyflowsResponse")
-public class ShowAddMoneyflowsResponse extends AbstractAddMoneyflowResponse {
+public class ShowAddMoneyflowsResponse {
+	@JsonProperty("preDefMoneyflowTransport")
+	private List<PreDefMoneyflowTransport> preDefMoneyflowTransports;
+	@JsonProperty("capitalsourceTransport")
+	private List<CapitalsourceTransport> capitalsourceTransports;
+	@JsonProperty("contractpartnerTransport")
+	private List<ContractpartnerTransport> contractpartnerTransports;
+	@JsonProperty("postingAccountTransport")
+	private List<PostingAccountTransport> postingAccountTransports;
+
+	public final List<PreDefMoneyflowTransport> getPreDefMoneyflowTransports() {
+		return this.preDefMoneyflowTransports;
+	}
+
+	public final void setPreDefMoneyflowTransports(final List<PreDefMoneyflowTransport> preDefMoneyflowTransports) {
+		this.preDefMoneyflowTransports = preDefMoneyflowTransports;
+	}
+
+	public final List<CapitalsourceTransport> getCapitalsourceTransports() {
+		return this.capitalsourceTransports;
+	}
+
+	public final void setCapitalsourceTransports(final List<CapitalsourceTransport> capitalsourceTransports) {
+		this.capitalsourceTransports = capitalsourceTransports;
+	}
+
+	public final List<ContractpartnerTransport> getContractpartnerTransports() {
+		return this.contractpartnerTransports;
+	}
+
+	public final void setContractpartnerTransports(final List<ContractpartnerTransport> contractpartnerTransports) {
+		this.contractpartnerTransports = contractpartnerTransports;
+	}
+
+	public final List<PostingAccountTransport> getPostingAccountTransports() {
+		return this.postingAccountTransports;
+	}
+
+	public final void setPostingAccountTransports(final List<PostingAccountTransport> postingAccountTransports) {
+		this.postingAccountTransports = postingAccountTransports;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((this.capitalsourceTransports == null) ? 0 : this.capitalsourceTransports.hashCode());
+		result = prime * result
+				+ ((this.contractpartnerTransports == null) ? 0 : this.contractpartnerTransports.hashCode());
+		result = prime * result
+				+ ((this.postingAccountTransports == null) ? 0 : this.postingAccountTransports.hashCode());
+		result = prime * result
+				+ ((this.preDefMoneyflowTransports == null) ? 0 : this.preDefMoneyflowTransports.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final ShowAddMoneyflowsResponse other = (ShowAddMoneyflowsResponse) obj;
+		if (this.capitalsourceTransports == null) {
+			if (other.capitalsourceTransports != null) {
+				return false;
+			}
+		} else if (!this.capitalsourceTransports.equals(other.capitalsourceTransports)) {
+			return false;
+		}
+		if (this.contractpartnerTransports == null) {
+			if (other.contractpartnerTransports != null) {
+				return false;
+			}
+		} else if (!this.contractpartnerTransports.equals(other.contractpartnerTransports)) {
+			return false;
+		}
+		if (this.postingAccountTransports == null) {
+			if (other.postingAccountTransports != null) {
+				return false;
+			}
+		} else if (!this.postingAccountTransports.equals(other.postingAccountTransports)) {
+			return false;
+		}
+		if (this.preDefMoneyflowTransports == null) {
+			if (other.preDefMoneyflowTransports != null) {
+				return false;
+			}
+		} else if (!this.preDefMoneyflowTransports.equals(other.preDefMoneyflowTransports)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("ShowAddMoneyflowsResponse [preDefMoneyflowTransports=");
+		builder.append(this.preDefMoneyflowTransports);
+		builder.append(", capitalsourceTransports=");
+		builder.append(this.capitalsourceTransports);
+		builder.append(", contractpartnerTransports=");
+		builder.append(this.contractpartnerTransports);
+		builder.append(", postingAccountTransports=");
+		builder.append(this.postingAccountTransports);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
