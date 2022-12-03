839b847fcf8e0eae2d3d85227ce50694fba52c4b
Oliver Lehmann <lehmann@ans-netz.de>
2022-12-04 00:09:37 +0100

    more support for the VueJS Client

---
+++ moneyjinn-core/src/main/java/org/laladev/moneyjinn/core/rest/model/contractpartneraccount/CreateContractpartnerAccountResponse.java
@@ -0,0 +1,85 @@
+package org.laladev.moneyjinn.core.rest.model.contractpartneraccount;
+
+//
+//Copyright (c) 2015 Oliver Lehmann <lehmann@ans-netz.de>
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+//
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.laladev.moneyjinn.core.rest.model.ValidationResponse;
+
+@XmlRootElement(name = "createContractpartnerAccountResponse")
+public class CreateContractpartnerAccountResponse extends ValidationResponse {
+	private Long contractpartnerAccountId;
+
+	public final Long getcontractpartnerAccountId() {
+		return this.contractpartnerAccountId;
+	}
+
+	public final void setcontractpartnerAccountId(final Long contractpartnerAccountId) {
+		this.contractpartnerAccountId = contractpartnerAccountId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((this.contractpartnerAccountId == null) ? 0 : this.contractpartnerAccountId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final CreateContractpartnerAccountResponse other = (CreateContractpartnerAccountResponse) obj;
+		if (this.contractpartnerAccountId == null) {
+			if (other.contractpartnerAccountId != null) {
+				return false;
+			}
+		} else if (!this.contractpartnerAccountId.equals(other.contractpartnerAccountId)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("CreateContractpartnerAccountResponse [contractpartnerAccountId=");
+		builder.append(this.contractpartnerAccountId);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
