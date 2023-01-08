a437ad61916f085a4c4b11279924400e2f3c81cb
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-08 22:19:24 +0100

    use jsr310 module for jackson

--- moneyjinn-hbci-batch/src/main/java/org/laladev/moneyjinn/hbci/batch/config/MyObjectMapper.java
+++ moneyjinn-hbci-batch/src/main/java/org/laladev/moneyjinn/hbci/batch/config/MyObjectMapper.java
@@ -5,18 +5,21 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationIntrospector;
 
 public class MyObjectMapper extends ObjectMapper {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	public MyObjectMapper() {
-		super();
-		super.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
-		super.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
-		super.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		super.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-		super.setAnnotationIntrospector(new JakartaXmlBindAnnotationIntrospector(TypeFactory.defaultInstance()));
-	}
+  public MyObjectMapper() {
+    super();
+    super.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+    super.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
+    super.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    super.setAnnotationIntrospector(
+        new JakartaXmlBindAnnotationIntrospector(TypeFactory.defaultInstance()));
+    super.registerModule(new JavaTimeModule());
+
+  }
 }
