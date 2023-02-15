819d3b40b398bca87d4847d75da7616c01d3db1d
Oliver Lehmann <lehmann@ans-netz.de>
2023-02-15 17:21:30 +0100

    fix using default value for not null columns #42

--- moneyjinn-hbci-batch/src/main/java/org/laladev/moneyjinn/hbci/core/entity/BalanceDaily.java
+++ moneyjinn-hbci-batch/src/main/java/org/laladev/moneyjinn/hbci/core/entity/BalanceDaily.java
@@ -25,109 +25,109 @@
 //
 package org.laladev.moneyjinn.hbci.core.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
 @Entity
 @Table(name = "balance_daily")
 public class BalanceDaily extends AbstractAccountEntitiy implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private LocalDate balanceDate;
-	private LocalDateTime lastTransactionDate;
-	private BigDecimal balanceAvailableValue;
-	private BigDecimal lineOfCreditValue;
-	private String balanceCurrency;
-	private LocalDateTime lastBalanceUpdate;
+  private static final long serialVersionUID = 1L;
+  private LocalDate balanceDate;
+  private LocalDateTime lastTransactionDate;
+  private BigDecimal balanceAvailableValue;
+  private BigDecimal lineOfCreditValue;
+  private String balanceCurrency;
+  private LocalDateTime lastBalanceUpdate;
 
-	@Column(name = "balance_date")
-	public LocalDate getBalanceDate() {
-		return this.balanceDate;
-	}
+  @Column(name = "balance_date")
+  public LocalDate getBalanceDate() {
+    return this.balanceDate;
+  }
 
-	public void setBalanceDate(final LocalDate balanceDate) {
-		this.balanceDate = balanceDate;
-	}
+  public void setBalanceDate(final LocalDate balanceDate) {
+    this.balanceDate = balanceDate;
+  }
 
-	@Column(name = "last_transaction_date")
-	public LocalDateTime getLastTransactionDate() {
-		return this.lastTransactionDate;
-	}
+  @Column(name = "last_transaction_date", insertable = false)
+  public LocalDateTime getLastTransactionDate() {
+    return this.lastTransactionDate;
+  }
 
-	public void setLastTransactionDate(final LocalDateTime lastTransactionDate) {
-		this.lastTransactionDate = lastTransactionDate;
-	}
+  public void setLastTransactionDate(final LocalDateTime lastTransactionDate) {
+    this.lastTransactionDate = lastTransactionDate;
+  }
 
-	@Column(name = "balance_available_value")
-	public BigDecimal getBalanceAvailableValue() {
-		return this.balanceAvailableValue;
-	}
+  @Column(name = "balance_available_value")
+  public BigDecimal getBalanceAvailableValue() {
+    return this.balanceAvailableValue;
+  }
 
-	public void setBalanceAvailableValue(final BigDecimal balanceAvailableValue) {
-		this.balanceAvailableValue = balanceAvailableValue;
-	}
+  public void setBalanceAvailableValue(final BigDecimal balanceAvailableValue) {
+    this.balanceAvailableValue = balanceAvailableValue;
+  }
 
-	@Column(name = "line_of_credit_value")
-	public BigDecimal getLineOfCreditValue() {
-		return this.lineOfCreditValue;
-	}
+  @Column(name = "line_of_credit_value")
+  public BigDecimal getLineOfCreditValue() {
+    return this.lineOfCreditValue;
+  }
 
-	public void setLineOfCreditValue(final BigDecimal lineOfCreditValue) {
-		this.lineOfCreditValue = lineOfCreditValue;
-	}
+  public void setLineOfCreditValue(final BigDecimal lineOfCreditValue) {
+    this.lineOfCreditValue = lineOfCreditValue;
+  }
 
-	@Column(name = "balance_currency")
-	public String getBalanceCurrency() {
-		return this.balanceCurrency;
-	}
+  @Column(name = "balance_currency")
+  public String getBalanceCurrency() {
+    return this.balanceCurrency;
+  }
 
-	public void setBalanceCurrency(final String balanceCurrency) {
-		this.balanceCurrency = balanceCurrency;
-	}
+  public void setBalanceCurrency(final String balanceCurrency) {
+    this.balanceCurrency = balanceCurrency;
+  }
 
-	@Column(name = "last_balance_update")
-	public LocalDateTime getLastBalanceUpdate() {
-		return this.lastBalanceUpdate;
-	}
+  @Column(name = "last_balance_update", insertable = false)
+  public LocalDateTime getLastBalanceUpdate() {
+    return this.lastBalanceUpdate;
+  }
 
-	public void setLastBalanceUpdate(final LocalDateTime lastBalanceUpdate) {
-		this.lastBalanceUpdate = lastBalanceUpdate;
-	}
+  public void setLastBalanceUpdate(final LocalDateTime lastBalanceUpdate) {
+    this.lastBalanceUpdate = lastBalanceUpdate;
+  }
 
-	@Override
-	public boolean equals(final Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof BalanceDaily)) {
-			return false;
-		}
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof BalanceDaily)) {
+      return false;
+    }
 
-		final BalanceDaily that = (BalanceDaily) o;
+    final BalanceDaily that = (BalanceDaily) o;
 
-		return super.equals(that);
-	}
+    return super.equals(that);
+  }
 
-	@Override
-	public int hashCode() {
-		int result = super.hashCode();
-		result = 31 * result + this.balanceDate.hashCode();
-		return result;
-	}
+  @Override
+  public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + this.balanceDate.hashCode();
+    return result;
+  }
 
-	@Override
-	public String toString() {
-		return "BalanceDaily{" + "id=" + super.getId() + ", myIban='" + super.getMyIban() + '\'' + ", myBic='"
-				+ super.getMyBic() + '\'' + ", myAccountnumber=" + super.getMyAccountnumber() + ", myBankcode="
-				+ super.getMyBankcode() + ", balanceDate=" + this.balanceDate + ", lastTransactionDate="
-				+ this.lastTransactionDate + ", balanceAvailableValue=" + this.balanceAvailableValue
-				+ ", lineOfCreditValue=" + this.lineOfCreditValue + ", balanceCurrency='" + this.balanceCurrency + '\''
-				+ ", lastBalanceUpdate=" + this.lastBalanceUpdate + '}';
-	}
+  @Override
+  public String toString() {
+    return "BalanceDaily{" + "id=" + super.getId() + ", myIban='" + super.getMyIban() + '\''
+        + ", myBic='" + super.getMyBic() + '\'' + ", myAccountnumber=" + super.getMyAccountnumber()
+        + ", myBankcode=" + super.getMyBankcode() + ", balanceDate=" + this.balanceDate
+        + ", lastTransactionDate=" + this.lastTransactionDate + ", balanceAvailableValue="
+        + this.balanceAvailableValue + ", lineOfCreditValue=" + this.lineOfCreditValue
+        + ", balanceCurrency='" + this.balanceCurrency + '\'' + ", lastBalanceUpdate="
+        + this.lastBalanceUpdate + '}';
+  }
 }
